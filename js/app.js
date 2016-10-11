125486e8fb2ddd1b6549543ec7c83d3bed212dd4
Nick Lanza <NickLanza88@gmail.com>
2016-10-10 20:06:22 -0700

    Completed Task 4. On to regression with task 5

--- js/app.js
+++ js/app.js
@@ -1,11 +1,26 @@
 // (done) 1. Add a search bar and search button when the page loads
 // (done) 2. Add navigation at the bottom of the page
+
 $(document).ready(function(){
-  $('.page-header').append('<div class="student-search"><input type="search" name="search-box" placeholder="Search..."><button id="search-button">Search</button></div>');
-  $('.add').append('<div class="pagination"><ul><li class="button1"><a href="#">1</a></li><li class="button2"><a href="#">2</a></li><li class="button3"><a href="#">3</a></li><li class="button4"><a href="#">4</a></li></ul></div>');
+  $('.page-header').append('<div class="student-search"><input type="search" name="search-box" placeholder="Search..." id="searchAll"><button id="search-button">Search</button></div>');
+  $('.student-list').append('<div class="pagination"><ul><li><a href="#">1</a></li><li><a href="#">2</a></li><li><a href="#">3</a></li><li><a href="#">4</a></li><li><a href="#">5</a></li><li><a href="#">6</a></li></div>');
 });
 
-
+$(document).ready(function(){
+  $('.pagination ul li:nth-child(1)').addClass('button1');
+  $('.pagination ul li:nth-child(2)').addClass('button2');
+  $('.pagination ul li:nth-child(3)').addClass('button3');
+  $('.pagination ul li:nth-child(4)').addClass('button4');
+  $('.pagination ul li:nth-child(5)').addClass('button5');
+  $('.pagination ul li:nth-child(6)').addClass('button6');
+  button1();
+  button2();
+  button3();
+  button4();
+  button5();
+  button6();
+  searchAll();
+});
 //(done) 3. Hide all but the first 10 students when the page loads.
 
 var allStudents = $('ul.student-list').children().length;
@@ -20,17 +35,82 @@ function calculatePageLoad(){
 
 calculatePageLoad();
 
-//  4. When a user clicks on “2” in the pagination, students 11 through 20 are shown. When a user clicks “3”, students 21 through 30 are shown. And so on. When “6” is clicked 51 through 55 should be shown.
+// (done) 4. When a user clicks on “2” in the pagination, students 11 through 20 are shown. When a user clicks “3”, students 21 through 30 are shown. And so on. When “6” is clicked 51 through 55 should be shown.
 
-//trying to find where I need to click, so I can use the function below, but can't find it.
+function button1() {
+  $('.button1').on('click', function(){
+      for(var i = 0; i < allStudents; i++) {
+        if (i <= 9) {
+          $("li.student-item").eq(i).show();
+        } else if (i > 9) {
+          $("li.student-item").eq(i).hide();
+        }
+      }
+    });
+}
 
-var button2 = $('li.button2');
-console.log(button2);
+function button2() {
+  $('.button2').on('click', function(){
+      for(var i = 0; i < allStudents; i++) {
+        if (i <= 9 || i >= 20) {
+          $("li.student-item").eq(i).hide();
+        } else if (i > 9 || i < 20) {
+          $("li.student-item").eq(i).show();
+        }
+      }
+    });
+}
 
-button2.addEventlistener('click', function(){
-  for(var i = 0; i < allStudents; i++) {
-    if (i <= 9 || i > 19) {
-      $("li.student-item").eq(i).hide();
+function button3() {
+  $('.button3').on('click', function(){
+    for(var i = 0; i < allStudents; i++) {
+      if (i <= 19 || i >= 30) {
+        $("li.student-item").eq(i).hide();
+      } else if (i > 19 || i < 31) {
+        $("li.student-item").eq(i).show();
+      }
     }
-  }
-});
+  });
+}
+
+function button4() {
+  $('.button4').on('click', function(){
+    for(var i = 0; i < allStudents; i++) {
+      if (i <= 29 || i >=40) {
+        $("li.student-item").eq(i).hide();
+      } else if (i > 29 || i < 41) {
+        $("li.student-item").eq(i).show();
+      }
+    }
+  });
+}
+function button5() {
+  $('.button5').on('click', function(){
+    for(var i = 0; i < allStudents; i++) {
+      if (i <= 39 || i >= 50) {
+        $("li.student-item").eq(i).hide();
+      } else if (i >= 39 || i < 51) {
+        $("li.student-item").eq(i).show();
+      }
+    }
+  });
+}
+function button6() {
+  $('.button6').on('click', function(){
+    for(var i = 0; i < allStudents; i++) {
+      if (i <= 49) {
+        $("li.student-item").eq(i).hide();
+      } else if (i >= 50) {
+        $("li.student-item").eq(i).show();
+      }
+    }
+  });
+}
+
+// 5. Add an event listener to the search button. When the user clicks on the button it should use the text in the search input to filter the results. Searching should be case insensitive. e.g. a search for “Susan” should return results for “susan” and “Susan".
+
+function searchAll(){
+  $('#searchAll').on('click',function(){
+
+  });
+}
